ae61823d79a7b8be7265f812bc6892f9ae7d3afd
cwong38 <cwong38@illinois.edu>
2015-02-10 12:40:00 +0800

    nooooo

--- hellowworld/src/hellowworld/helloworld.java
+++ hellowworld/src/hellowworld/helloworld.java
@@ -2,6 +2,6 @@ package hellowworld;
 
 public class helloworld {
 	public static void main(String[] args){
-		System.out.print("they see me rolling\n they hating.");
+		System.out.print("they see me rolling\n they hating.");//noooooo
 	}
 }
